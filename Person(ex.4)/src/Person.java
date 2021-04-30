fd24503b60420cae9b064496c0759e427a2be7bf
Ksenia <nitherland>
2021-04-30 16:30:27 +0300

    New task(ex.4)

---
+++ Person(ex.4)/src/Person.java
@@ -0,0 +1,21 @@
+import java.text.FieldPosition;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Person {
+    private Date bdate;
+
+    public Person(Date date){
+        bdate = date;
+    }
+
+    public String toString(SimpleDateFormat format) {
+        StringBuffer result = new StringBuffer();
+        System.out.println(format);
+        System.out.println(format.format(bdate));
+        result = format.format(bdate, result, new FieldPosition(0));
+        return result.toString();
+    }
+}
